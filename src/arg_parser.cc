f4b85a83765e0831000c92736cc134139c2c0933
Joy Yeh <joyyeh.tw@gmail.com>
2018-12-14 01:48:08 +0900

    Formatting changes

--- src/arg_parser.cc
+++ src/arg_parser.cc
@@ -7,57 +7,9 @@
 
 namespace elfin {
 
-/* Private Methods */
+/* private */
 
-ARG_PARSER_CALLBACK(help_and_exit) {
-    raw("elfin: An Automatic Protein Designer\n");
-    raw("Usage: ./elfin [OPTIONS]\n");
-    raw("Note: Setting an option will override the same setting in the json file\n");
-    raw("Help:\n");
-    print_args();
-    exit(1);
-}
-
-ARG_PARSER_CALLBACK(failure_callback) {
-    printf("Argument parsing failed on string: \"%s\"\n", arg_in.c_str());
-    help_and_exit();
-    exit(1);
-}
-
-ARG_PARSER_CALLBACK(parse_config) {
-    options_.config_file = arg_in;
-
-    NICE_PANIC(!file_exists(options_.config_file.c_str()),
-               string_format("Settings file does not exist: \"%s\"\n",
-                             options_.config_file.c_str()));
-
-    const JSON j = parse_json(options_.config_file);
-
-    for (auto it = j.begin(); it != j.end(); ++it) {
-        const std::string opt_name = "--" + it.key();
-        auto ab = match_arg_bundle(opt_name.c_str());
-        if (ab) {
-            (this->*ab->callback)(json_to_str(j[ab->long_form]));
-        } else {
-            die("Unrecognized option: %s\n", opt_name.c_str());
-        }
-    }
-}
-
-ARG_PARSER_CALLBACK(set_input_file) {
-    options_.input_file = arg_in;
-}
-
-void ArgParser::print_args() const {
-    for (const auto& ab : argb_) {
-        set_leading_spaces(8);
-        raw("%s, %s\n", ab.short_form.c_str(), ab.long_form.c_str());
-        set_leading_spaces(12);
-        raw("%s\n", ab.description.c_str());
-    }
-    reset_leading_spaces();
-}
-
+/* accessors */
 const ArgBundle* ArgParser::match_arg_bundle(const char *arg_in) const {
     // anything shorter than 2 chars cannot match
     if (arg_in[0] == '-')
@@ -72,32 +24,6 @@ const ArgBundle* ArgParser::match_arg_bundle(const char *arg_in) const {
     }
 
     return nullptr;
-}
-
-void ArgParser::parse_options(const int argc, char const *argv[]) {
-    for (size_t i = 1; i < argc; i++) {
-        // iterate through argument bundle to match argument
-        auto arg = argv[i];
-        auto ab = match_arg_bundle(arg);
-        if (ab) {
-            if (ab->exp_val) {
-                if (i + 1 > argc - 1) {
-                    err("Argument %s expects to be followed by a value\n", arg);
-                    failure_callback(arg);
-                }
-                else {
-                    (this->*ab->callback)(argv[++i]); // passes next arg string
-                }
-            }
-            else {
-                (this->*ab->callback)(arg); // passes current arg name
-            }
-        }
-        else {
-            err("Unknown argument: %s\n", arg);
-            failure_callback(arg);
-        }
-    }
 }
 
 void ArgParser::check_options() const {
@@ -147,6 +73,83 @@ void ArgParser::check_options() const {
     NICE_PANIC(options_.keep_n < 0 ||
                options_.keep_n > options_.ga_pop_size,
                "Number of best solutions to output must be > 0 and < ga_pop_size");
+}
+
+/* modifiers */
+void ArgParser::parse_options(const int argc, char const *argv[]) {
+    for (size_t i = 1; i < argc; ++i) {
+        // iterate through argument bundle to match argument
+        auto arg = argv[i];
+        auto ab = match_arg_bundle(arg);
+        if (ab) {
+            if (ab->exp_val) {
+                if (i + 1 > argc - 1) {
+                    err("Argument %s expects to be followed by a value\n", arg);
+                    failure_callback(arg);
+                }
+                else {
+                    (this->*ab->callback)(argv[++i]); // passes next arg string
+                }
+            }
+            else {
+                (this->*ab->callback)(arg); // passes current arg name
+            }
+        }
+        else {
+            err("Unknown argument: %s\n", arg);
+            failure_callback(arg);
+        }
+    }
+}
+
+ARG_PARSER_CALLBACK(help_and_exit) {
+    raw("elfin: An Automatic Protein Designer\n");
+    raw("Usage: ./elfin [OPTIONS]\n");
+    raw("Note: Setting an option will override the same setting in the json file\n");
+    raw("Help:\n");
+    print_args();
+    exit(1);
+}
+
+ARG_PARSER_CALLBACK(failure_callback) {
+    printf("Argument parsing failed on string: \"%s\"\n", arg_in.c_str());
+    help_and_exit();
+    exit(1);
+}
+
+ARG_PARSER_CALLBACK(parse_config) {
+    options_.config_file = arg_in;
+
+    NICE_PANIC(!file_exists(options_.config_file.c_str()),
+               string_format("Settings file does not exist: \"%s\"\n",
+                             options_.config_file.c_str()));
+
+    const JSON j = parse_json(options_.config_file);
+
+    for (auto it = j.begin(); it != j.end(); ++it) {
+        const std::string opt_name = "--" + it.key();
+        auto ab = match_arg_bundle(opt_name.c_str());
+        if (ab) {
+            (this->*ab->callback)(json_to_str(j[ab->long_form]));
+        } else {
+            die("Unrecognized option: %s\n", opt_name.c_str());
+        }
+    }
+}
+
+ARG_PARSER_CALLBACK(set_input_file) {
+    options_.input_file = arg_in;
+}
+
+/* printers */
+void ArgParser::print_args() const {
+    for (const auto& ab : argb_) {
+        set_leading_spaces(8);
+        raw("%s, %s\n", ab.short_form.c_str(), ab.long_form.c_str());
+        set_leading_spaces(12);
+        raw("%s\n", ab.description.c_str());
+    }
+    reset_leading_spaces();
 }
 
 /* public */
