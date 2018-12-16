72131492b06266f86bcce9d9d5486fcfa9e48403
Joy Yeh <joyyeh.tw@gmail.com>
2018-12-17 00:10:16 +0900

    Tested Eigen matrix based Transform vs array based implementation. Array is ~20% faster.

--- headers/transform.h
+++ headers/transform.h
@@ -1,41 +1,121 @@
-#ifndef TRANSFORM_H_
-#define TRANSFORM_H_
+#ifndef TRANSFOREIGEN_H_
+#define TRANSFOREIGEN_H_
 
+// #define USE_EIGEN_IMPL
+
+#include <sstream>
+
+#ifdef USE_EIGEN_IMPL
 #include "eigen.h"
+#endif  /* ifdef USE_EIGEN_IMPL */
+
+
 #include "json.h"
+
 
 namespace elfin {
 
 class Vector3f;
 
-typedef Eigen::Matrix4f EigenTransform;
-class Transform : public EigenTransform {
+#ifdef USE_EIGEN_IMPL
+using Matrix4f = Eigen::Matrix4f;
+#define TX_INHERIT : public Matrix4f
+#else
+#define TX_INHERIT
+#endif  /* ifdef USE_EIGEN_IMPL */
+
+class Transform TX_INHERIT {
+private:
+
+#ifndef USE_EIGEN_IMPL
+
+    /* data */
+    float rot_[3][3] = {
+        {1, 0, 0},
+        {0, 1, 0},
+        {0, 0, 1}
+    };
+
+    float tran_[3] = {0, 0, 0};
+
+#endif  /* ifndef USE_EIGEN_IMPL */
+
 public:
+
     /* ctors */
+#ifdef USE_EIGEN_IMPL
 
     // Construct Transform from Eigen expressions
     template<typename OtherDerived>
     Transform(
         Eigen::MatrixBase<OtherDerived> const& other)
-        : EigenTransform(other) {}
-    Transform() : EigenTransform(Identity()) {}
+        : Matrix4f(other) {}
+    Transform() : Matrix4f(Identity()) {}
+
+#else
+
+    Transform() {}
+
+#endif  /* ifdef USE_EIGEN_IMPL */
+
     Transform(JSON const& tx_json);
 
     /* accessors */
     Vector3f collapsed() const;
     Transform inversed() const;
-    Transform operator*(Transform const& rhs) const;
-    Vector3f operator*(Vector3f const& vec) const;
+
+#ifdef USE_EIGEN_IMPL
+    template<typename OtherDerived>
+    Transform operator*(
+        Eigen::MatrixBase<OtherDerived> const & rhs) const {
+        return Matrix4f::operator*(rhs);
+    }
+#else
+    Transform operator*(Transform const & rhs) const;
+#endif  /* ifdef USE_EIGEN_IMPL */
+
+    Vector3f operator*(Vector3f const & vec) const;
+    bool is_approx(Transform const& other) const;
 
     /* modifiers */
+#ifdef USE_EIGEN_IMPL
     // Assign Eigen expressions to Transform
     template<typename OtherDerived>
-    Transform& operator=(
+    Transform & operator=(
         Eigen::MatrixBase<OtherDerived> const& other)
     {
-        this->EigenTransform::operator=(other);
+        this->Matrix4f::operator=(other);
         return *this;
     }
+#endif  /* ifdef USE_EIGEN_IMPL */
+
+    /* printers */
+#ifndef USE_EIGEN_IMPL
+
+    std::string to_string() const {
+        std::ostringstream ss;
+
+        ss << "Transform[\nRot[";
+        for (size_t i = 0; i < 3; ++i) {
+            ss << "  [ ";
+            for (size_t j = 0; j < 3; ++j) {
+                ss << rot_[i][j];
+                if (j < 3) {
+                    ss << ", ";
+                }
+            }
+            ss << " ]\n";
+        }
+        ss << "]\n";
+
+        ss << "Tran[" << tran_[0];
+        ss << ", " << tran_[1];
+        ss << ", " << tran_[2] << "]";
+
+        return ss.str();
+    }
+
+#endif  /* ifndef USE_EIGEN_IMPL */
 
     /* tests */
     static void test(size_t& errors, size_t& tests);
@@ -43,4 +123,6 @@ public:
 
 }  /* elfin */
 
-#endif  /* end of include guard: TRANSFORM_H_ */
+#undef TX_IMPL
+
+#endif  /* end of include guard: TRANSFORM_EIGEN_IMPL_H_ */
--- src/transform.cc
+++ src/transform.cc
@@ -7,44 +7,168 @@ namespace elfin {
 
 /* ctors */
 Transform::Transform(JSON const& tx_json) {
-    JSON const& rot = tx_json["rot"];
-    NICE_PANIC(rot.size() != 3);
-    NICE_PANIC(rot[0].size() != 3);
+    JSON const& rot_json = tx_json["rot"];
+    NICE_PANIC(rot_json.size() != 3);
+    NICE_PANIC(rot_json[0].size() != 3);
 
-    JSON const& tran = tx_json["tran"];
-    NICE_PANIC(tran.size() != 3);
+    JSON const& tran_json = tx_json["tran"];
+    NICE_PANIC(tran_json.size() != 3);
 
-    (*this) << rot[0][0], rot[0][1], rot[0][2], tran[0],
-    rot[1][0], rot[1][1], rot[1][2], tran[1],
-    rot[2][0], rot[2][1], rot[2][2], tran[2],
+#ifdef USE_EIGEN_IMPL
+
+    (*this) << rot_json[0][0], rot_json[0][1], rot_json[0][2], tran_json[0],
+    rot_json[1][0], rot_json[1][1], rot_json[1][2], tran_json[1],
+    rot_json[2][0], rot_json[2][1], rot_json[2][2], tran_json[2],
     0.f, 0.f, 0.f, 1.f;
+
+#else
+
+    for (size_t i = 0; i < 3; ++i) {
+        for (size_t j = 0; j < 3; ++j) {
+            rot_[i][j] = rot_json[i][j];
+        }
+        tran_[i] = tran_json[i];
+    }
+
+#endif  /* ifdef USE_EIGEN_IMPL */
+
 }
 
 /* accessors */
 Vector3f Transform::collapsed() const {
+#ifdef USE_EIGEN_IMPL
+
     return block<3, 1>(0, 3);
+
+#else
+
+    return Vector3f(tran_[0], tran_[1], tran_[2]);
+
+#endif  /* ifdef USE_EIGEN_IMPL */
 }
 
 Transform Transform::inversed() const {
-    Eigen::Matrix3f const inv_rot =
-        block<3, 3>(0, 0).transpose();
-    Eigen::Vector3f const inv_tran =
-        inv_rot * -block<3, 1>(0, 3);
+#ifdef USE_EIGEN_IMPL
+
     Transform res;
-    res << inv_rot(0, 0), inv_rot(0, 1), inv_rot(0, 2), inv_tran(0),
-        inv_rot(1, 0), inv_rot(1, 1), inv_rot(1, 2), inv_tran(1),
-        inv_rot(2, 0), inv_rot(2, 1), inv_rot(2, 2), inv_tran(2),
-        0, 0, 0, 1;
+
+    res.block<3, 3>(0, 0) = block<3, 3>(0, 0).transpose();
+    res.block<3, 1>(0, 3) = -(res.block<3, 3>(0, 0) * block<3, 1>(0, 3));
+
+    return res;
+
+#else
+
+    Transform res;
+
+    for (size_t i = 0; i < 3; ++i) {
+        for (size_t j = 0; j < 3; ++j) {
+            res.rot_[i][j] = rot_[j][i];
+        }
+    }
+
+    res.tran_[0] = -(res.rot_[0][0] * tran_[0] +
+                     res.rot_[0][1] * tran_[1] +
+                     res.rot_[0][2] * tran_[2]);
+
+    res.tran_[1] = -(res.rot_[1][0] * tran_[0] +
+                     res.rot_[1][1] * tran_[1] +
+                     res.rot_[1][2] * tran_[2]);
+
+    res.tran_[2] = -(res.rot_[2][0] * tran_[0] +
+                     res.rot_[2][1] * tran_[1] +
+                     res.rot_[2][2] * tran_[2]);
+
+    return res;
+
+#endif  /* ifdef USE_EIGEN_IMPL */
+}
+
+#ifndef USE_EIGEN_IMPL
+
+Transform Transform::operator*(Transform const& rhs) const {
+    Transform res;
+
+    for (size_t i = 0; i < 3; ++i) {
+        for (size_t j = 0; j < 3; ++j) {
+            res.rot_[i][j] =
+                rot_[i][0] * rhs.rot_[0][j] +
+                rot_[i][1] * rhs.rot_[1][j] +
+                rot_[i][2] * rhs.rot_[2][j];
+        }
+        res.tran_[i] =
+            rot_[i][0] * rhs.tran_[0] +
+            rot_[i][1] * rhs.tran_[1] +
+            rot_[i][2] * rhs.tran_[2] + tran_[i] ;
+    }
+
     return res;
 }
 
-Transform Transform::operator*(Transform const& rhs) const {
-    return EigenTransform::operator*(rhs);
-}
+#endif  /* ifndef USE_EIGEN_IMPL */
 
 Vector3f Transform::operator*(
     Vector3f const& vec) const {
+#ifdef USE_EIGEN_IMPL
+
     return block<3, 3>(0, 0) * vec + block<3, 1>(0, 3);
+
+#else
+
+    float const rx = rot_[0][0] * vec[0] +
+                     rot_[0][1] * vec[1] +
+                     rot_[0][2] * vec[2] +
+                     tran_[0];
+
+    float const ry = rot_[1][0] * vec[0] +
+                     rot_[1][1] * vec[1] +
+                     rot_[1][2] * vec[2] +
+                     tran_[1];
+
+    float const rz = rot_[2][0] * vec[0] +
+                     rot_[2][1] * vec[1] +
+                     rot_[2][2] * vec[2] +
+                     tran_[2];
+
+    return Vector3f(rx, ry, rz);
+
+#endif  /* ifdef USE_EIGEN_IMPL */
+}
+
+bool Transform::is_approx(Transform const& other) const {
+#ifdef USE_EIGEN_IMPL
+
+    return isApprox(other);
+
+#else
+
+    /*
+     * We use 0.0001 as tolerance here because that's the highest precision
+     * PDBs support. 
+     */
+    float const tolerance = 1e-4;
+
+    for (size_t i = 0; i < 3; ++i) {
+        for (size_t j = 0; j < 3; ++j) {
+            if (not float_approximates_err(
+                        rot_[i][j],
+                        other.rot_[i][j],
+                        tolerance)) {
+                return false;
+            }
+        }
+
+        if (not float_approximates_err(
+                    tran_[i],
+                    other.tran_[i],
+                    tolerance)) {
+            return false;
+        }
+    }
+
+    return true;
+
+#endif  /* ifdef USE_EIGEN_IMPL */
 }
 
 /* tests */
