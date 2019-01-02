353d6edfeb611a2345b24ccd6fb57e7685c366ac
Joy Yeh <joyyeh.tw@gmail.com>
2019-01-02 17:38:39 +0900

    Fixed integration tests not testing multple work sites. Added allow_partial for those that do not match full recipes

--- src/evolution_solver_test.cc
+++ src/evolution_solver_test.cc
@@ -13,13 +13,15 @@ TestStat EvolutionSolver::test() {
 
     // Test expected solution.
     auto test_fragment =
-        [&](std::string const & spec, tests::Recipe const & recipe)
+        [&](std::string const & spec_file,
+            tests::Recipe const & recipe,
+            bool const allow_partial = false)
     {
         ts.tests++;
 
         // Force 1 thread for reproduciblility.
         InputManager::load_test_config(
-            /*spec_file=*/ spec,
+            spec_file,
             /*n_workers=*/ 1);
 
 
@@ -30,55 +32,57 @@ TestStat EvolutionSolver::test() {
         solver.run();
         JUtil.set_log_lvl(original_ll);
 
-        auto const& [work_area_name, work_area] = *begin(SPEC.work_areas());
-        auto const& solutions = solver.best_sols(work_area_name);  // TeamPtrMaxHeap
+        for (auto const& [work_area_name, work_area] : SPEC.work_areas()) {
+            auto const& solutions = solver.best_sols(work_area_name);  // TeamPtrMaxHeap
 
-        try { // Catch bad_cast
-            auto best_pt =
-                static_cast<PathTeam const&>(*solutions.top());
-            auto path_gen = best_pt.gen_path();
+            try { // Catch bad_cast
+                auto best_pt =
+                    static_cast<PathTeam const&>(*solutions.top());
+                auto path_gen = best_pt.gen_path();
 
-            // Might need to reverse recipe because data exported from
-            // elfin-ui does not gurantee consistent starting tip.
-            bool const should_reverse = recipe[0].mod_name !=
-                                        path_gen.peek()->prototype_->name;
+                // Might need to reverse recipe because data exported from
+                // elfin-ui does not gurantee consistent starting tip.
+                bool const should_reverse = recipe[0].mod_name !=
+                                            path_gen.peek()->prototype_->name;
 
-            auto recipe_fwd = should_reverse ?
-                              tests::Recipe(rbegin(recipe), rend(recipe)) :
-                              recipe;
-            auto step_itr = begin(recipe_fwd);
+                auto recipe_fwd = should_reverse ?
+                                  tests::Recipe(rbegin(recipe), rend(recipe)) :
+                                  recipe;
+                auto step_itr = begin(recipe_fwd);
 
-            while (not path_gen.is_done()) {
-                auto node = path_gen.next();
-                if (step_itr == end(recipe_fwd) or
-                        step_itr->mod_name != node->prototype_->name) {
-                    ts.errors++;
-                    JUtil.error("Solver best solution differs from expectation.\n");
+                while (not path_gen.is_done()) {
+                    auto node = path_gen.next();
+                    if (step_itr == end(recipe_fwd) or
+                            (!allow_partial and step_itr->mod_name != node->prototype_->name)) {
+                        ts.errors++;
+                        JUtil.error("Solver fails to arrive at ideal solution for %s.\n",
+                                    spec_file.c_str());
 
-                    std::ostringstream sol_oss;
-                    sol_oss << "Solver solution:\n";
+                        std::ostringstream sol_oss;
+                        sol_oss << "Solver solution:\n";
 
-                    auto team_pg = best_pt.gen_path();
-                    while (not team_pg.is_done()) {
-                        sol_oss << team_pg.next()->prototype_->name << "\n";
+                        auto team_pg = best_pt.gen_path();
+                        while (not team_pg.is_done()) {
+                            sol_oss << team_pg.next()->prototype_->name << "\n";
+                        }
+                        JUtil.error(sol_oss.str().c_str());
+
+                        std::ostringstream exp_oss;
+                        exp_oss << "Expected solution:\n";
+                        for (auto& step : recipe_fwd) {
+                            exp_oss << step.mod_name << "\n";
+                        }
+
+                        JUtil.error(exp_oss.str().c_str());
+                        break;
                     }
-                    JUtil.error(sol_oss.str().c_str());
-
-                    std::ostringstream exp_oss;
-                    exp_oss << "Expected solution:\n";
-                    for (auto& step : recipe_fwd) {
-                        exp_oss << step.mod_name << "\n";
-                    }
-
-                    JUtil.error(exp_oss.str().c_str());
-                    break;
+                    ++step_itr;
                 }
-                ++step_itr;
             }
-        }
-        catch (std::bad_cast const& exp) {
-            ts.errors++;
-            JUtil.error("Bad cast in %s\n", __PRETTY_FUNCTION__);
+            catch (std::bad_cast const& exp) {
+                ts.errors++;
+                JUtil.error("Bad cast in %s\n", __PRETTY_FUNCTION__);
+            }
         }
     };
 
@@ -100,7 +104,8 @@ TestStat EvolutionSolver::test() {
 
     // Test 2x medium 1H type work area.
     test_fragment("examples/half_snake_2x1h.json",
-                  tests::half_snake_free_recipe);
+                  tests::half_snake_free_recipe,
+                  /*allow_partial=*/true);
 
     // Test short 2H type work area.
     // test_fragment("examples/H_2h.json",
